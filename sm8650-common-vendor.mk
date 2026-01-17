31f6af1c5546c6cc214d7d3b6b1a1968e937dace
anky894 <ko282ko82@gmail.com>
2026-01-17 11:49:34 +0530

    a

--- sm8650-common-vendor.mk
+++ sm8650-common-vendor.mk
@@ -640,6 +640,7 @@ PRODUCT_PACKAGES += \
     libvpplibrary \
     libvui_dmgr \
     libvui_dmgr_client \
+    libvui_intf \
     libwfdcodecv4l2_proprietary \
     libwfdcommonutils_proprietary \
     libwfdconfigutils_proprietary \
